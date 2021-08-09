15a747080a86f219d2851dea13619bafe6aee6d3
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-08-09 00:16:39 -0300

    add movingplatform

---
+++ Source/PuzzlePlatform/MovingPlatform.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	if (HasAuthority())
+	{
+		bReplicates = true;
+	}
+	
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void AMovingPlatform::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	if (!HasAuthority())
+	{
+		FVector Location = GetActorLocation();
+		Location += FVector(MoveSpeed * DeltaTime, 0, 0);
+
+		SetActorLocation(Location);
+	}
+}
