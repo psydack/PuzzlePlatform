0b1499ef91abfe4408a4f7162ba73788f1966d2e
Rofli Sanches <rofli.souza@insanemedia.com.br>
2021-08-11 23:39:16 -0300

    add instance with travel

---
+++ Source/PuzzlePlatform/PuzzlePlatformGameInstance.cpp
@@ -0,0 +1,41 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PuzzlePlatformGameInstance.h"
+#include "Engine/Engine.h"
+
+UPuzzlePlatformGameInstance::UPuzzlePlatformGameInstance(const FObjectInitializer& ObjectInitializer)
+{
+	UE_LOG(LogTemp, Warning, TEXT("CONSTRUCT"));
+}
+
+void UPuzzlePlatformGameInstance::Init()
+{
+	UE_LOG(LogTemp, Warning, TEXT("INIT"));
+}
+
+void UPuzzlePlatformGameInstance::Host()
+{
+	if (GEngine)
+	{
+		GEngine->AddOnScreenDebugMessage(0, 2, FColor::Green, TEXT("Hosting"));
+
+		UWorld* World = GetWorld();
+		if (!ensure(World != nullptr)) return;
+
+		World->ServerTravel("/Game/ThirdPersonCPP/Maps/ThirdPersonExampleMap?listen");
+	}
+}
+
+void UPuzzlePlatformGameInstance::Join(const FString& Address)
+{
+	UEngine* Engine = GetEngine();
+	if (!ensure(Engine != nullptr)) return;
+
+	Engine->AddOnScreenDebugMessage(0, 5, FColor::Green, FString::Printf(TEXT("Joining %s"), *Address));
+
+	APlayerController* PlayerController = GetFirstLocalPlayerController();
+	if (!ensure(PlayerController != nullptr)) return;
+
+	PlayerController->ClientTravel(Address, ETravelType::TRAVEL_Absolute);
+}
